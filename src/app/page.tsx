deac49487d71409a4537c81558ea802e12b0ca6e
Juan Cantero <juan.cantero.jarilla@gmail.com>
2025-05-11 17:42:54 +0200

    framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,38 @@
+"use client"
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <>
       {/* <Header alwaysVisible={false} /> */}
       <div className="mainBox flex items-center justify-center h-screen">
-        <div className="mainText text-center" style={{ maxWidth: "68vw", position: "relative", top: "-10vh" }}>
-          <p className="text-xl ">
+        <motion.div
+          className="mainText text-center"
+          style={{ maxWidth: "68vw", position: "relative", top: "-10vh" }}
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <motion.p
+            className="text-xl"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
             Descubre Marruecos con la Agencia Sentir Marruecos, líder en
             experiencias de viaje excepcionales. Sumérgete en la rica historia,
             los diversos paisajes y la vibrante cultura de Marruecos con
             nuestros tours cuidadosamente diseñados.
-          </p>
-          <p className="mainText2">
+          </motion.p>
+          <motion.p
+            className="mainText2"
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 1 }}
+          >
             ¡Viaja con nosotros y siente Marruecos como nunca antes!
-          </p>
-        </div>
+          </motion.p>
+        </motion.div>
       </div>
     </>
   );
