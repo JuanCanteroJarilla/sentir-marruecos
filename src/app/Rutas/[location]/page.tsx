87a02c6ac1a7fe805702f73db472299c8bc95ba7
Juan Cantero <juan.cantero.jarilla@gmail.com>
2025-05-23 20:16:43 +0200

    await props.params

--- src/app/Rutas/[location]/page.tsx
+++ src/app/Rutas/[location]/page.tsx
@@ -2,14 +2,14 @@ import "./styles.scss";
 import getRuta from "./getData";
 import RutaCliente from "../RutasClient";
 
-export default async function Ruta({ params }: { params: { location: string } }) {
-  const locationParam = params.location;
+export default async function Ruta(props: { params: Promise<{ location: string }> }) {
+  const { location } = await props.params;
   const response = await getRuta();
 
   return (
     <>
       <div className="mainDiv"></div>
-      <RutaCliente response={response} locationParam={locationParam} />
+      <RutaCliente response={response} locationParam={location} />
     </>
   );
 }
