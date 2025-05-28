8c44ce49c8c6b932f727c5bc8952a7ef1b4cc1be
Juan Cantero <juan.cantero.jarilla@gmail.com>
2025-05-28 13:54:19 +0200

    suspense wrapped

--- src/app/Rutas/page.tsx
+++ src/app/Rutas/page.tsx
@@ -3,21 +3,22 @@ import "./styles.scss";
 import Cards from "./cards";
 import { data } from "../data";
 import { useSearchParams } from "next/navigation";
-
+import { Suspense } from "react";
 
 type RutaKey = keyof typeof data;
 
-function CardRutas({}) {
+function CardRutas() {
   const searchParams = useSearchParams();
   const location = searchParams.get("location") as RutaKey | null;
 
   const rutas = location ? data[location] : Object.values(data).flat();
- 
 
   return (
     <>
       <div className="flex justify-center items-center pt-40 tituloRutas">
-        <p>Rutas desde {location}</p>
+        <p>
+          {location ? `Rutas desde ${location}` : "Todas las rutas disponibles"}
+        </p>
       </div>
       <div className="flex flex-wrap justify-center">
         {rutas.map((item) => (
@@ -28,4 +29,10 @@ function CardRutas({}) {
   );
 }
 
-export default CardRutas;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Cargando rutas...</div>}>
+      <CardRutas />
+    </Suspense>
+  );
+}
