3e38c1dc04e1608751ee6017023977baa2a8e3fb
Juan Cantero <juan.cantero.jarilla@gmail.com>
2025-06-02 12:12:09 +0200

    a veure ara....

--- src/app/Actividades/[activity]/page.tsx
+++ src/app/Actividades/[activity]/page.tsx
@@ -1,7 +1,12 @@
 import ActividadClient from "./ActivityClient";
 
+type PageProps = {
+  params: {
+    activity: string;
+  };
+};
 
-export default async function Page({ params }: { params: { activity: string } }) {
+export default async function Page({ params }: PageProps) {
   const { activity } = await params;
   return <ActividadClient activity={activity} />;
 }
