0f4fb5804af49565b1396a85eb2f3ac2d12f9c08
Juan Cantero <juan.cantero.jarilla@gmail.com>
2025-07-15 17:45:14 +0200

    wip - sobre nosotros

---
+++ src/app/Sobre-nosotros/page.tsx
@@ -0,0 +1,66 @@
+import "./styles.scss";
+import Image from "next/image";
+function SobreNosotros() {
+  return (
+    <>
+      <div className="mainDivSobreNosotros flex flex-col items-start h-screen">
+        <div className="bg-white mt-[20rem] max-w-[24rem] ml-4 sm:ml-16 md:ml-32 lg:ml-56">
+          <p className="titleSobreNosotros text-5xl">
+            Bienvenidos a Sentir Marruecos Viajes.
+          </p>
+          <p className="subtitleTitleSobreNosotros">
+            Esta agencia nace de años de experiencia, dedicación y un profundo
+            amor por la cultura y las tradiciones de nuestro país.
+          </p>
+        </div>
+      </div>
+      <div className="h-screen w-full flex flex-row justify-center gap-4">
+        <div className="mt-86 max-w-[60vw]">
+          <p className="salemText ">
+            Soy Salem, un bereber del desierto de Merzouga. Nací y crecí en el
+            corazón del Sáhara, en una familia nómada que ha recorrido sus dunas
+            durante generaciones. <br />
+            Mi infancia transcurrió entre caravanas de dromedarios, cielos
+            estrellados y la sabiduría de un pueblo que vive en armonía con la
+            tierra. Fue ahí donde aprendí a amar profundamente nuestra cultura y
+            la hospitalidad que nos define. Con el tiempo, me adentré en el
+            mundo del turismo. Trabajé en distintas áreas, conociendo cada
+            rincón del desierto, sus paisajes, sus historias y todo lo que hace
+            única a la cultura bereber. Esa experiencia despertó en mí el deseo
+            de crear algo propio. Así nació Sentir Marruecos Viajes, con la
+            misión de compartir con viajeros de todo el mundo la esencia y los
+            valores de mi tierra. Aquí, cada viaje es más que una ruta: es una
+            oportunidad de conectar con nuestra comunidad, nuestra historia y
+            nuestras tradiciones. Este proyecto es posible gracias a un equipo
+            excepcional, formado por amigos y locales que comparten mi pasión
+            por mostrar un Marruecos auténtico y cercano. También se han sumado
+            personas nuevas que aportan sus ganas, su energía, talento y el amor
+            por nuestra tierra.
+          </p>
+        </div>
+        <div className="mt-100">
+          <Image
+            src={"/images/salem.png"}
+            alt={`Rutas`}
+            width={400} // Ajusta el ancho según tu diseño
+            height={280} // Ajusta el alto según tu diseño
+            className="rounded-lg "
+          />
+        </div>
+      </div>
+      <div className="flex flex-row justify-center items-center mb-20 max-w-[60vw] mx-auto">
+        <p className="titleFooter text-2xl p-4">
+          Juntos, trabajamos para ofrecerte una experiencia real, cálida y
+          enriquecedora. Te invitamos a vivir esta aventura con nosotros. A
+          descubrir la magia de Marruecos, sus costumbres, su historia y la
+          belleza de sus paisajes. Queremos que cada momento se convierta en un
+          recuerdo inolvidable.
+          <br />
+          ¡Bienvenidos a Sentir Marruecos Viajes!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default SobreNosotros;
